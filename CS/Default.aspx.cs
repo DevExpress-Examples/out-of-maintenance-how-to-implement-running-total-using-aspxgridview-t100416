10c1e1a8306f8422bf25d182040132f12701eec9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.Web.ASPxGridView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+
+    }
+    protected void ASPxGridView1_CustomUnboundColumnData(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewColumnDataEventArgs e) {
+        ASPxGridView grid = (ASPxGridView)sender;
+        int visibleIndex = grid.FindVisibleIndexByKeyValue(e.GetListSourceFieldValue("ProductID"));
+        e.Value = (visibleIndex != 0) ?
+            Convert.ToInt32(grid.GetRowValues(visibleIndex, "UnitsInStock")) + Convert.ToInt32(grid.GetRowValues(visibleIndex - 1, "Total")) :
+            Convert.ToInt32(grid.GetRowValues(visibleIndex, "UnitsInStock"));
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to implement running total using ASPxGridView
+
+
+<p>This example demonstrates how to implement a running total summary using the ASPxGridView unbound column. In this example, we create an unbound column and handle the <a href="https://documentation.devexpress.com/#AspNet/DevExpressWebASPxGridViewASPxGridView_CustomUnboundColumnDatatopic">ASPxGridView.CustomUnboundColumnData</a> event. We calculate and assign the value to the cell on the server side.</p>
+
+<br/>
+
+
